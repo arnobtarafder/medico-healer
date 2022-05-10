e0e1c6fd0d7df7563a93f7a8c0e1c86b5cc9b8b3
arnobtarafder <rmtarnob@gmail.com>
2022-05-10 23:38:57 +0600

    add: navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './Pages/Authentication/Login/Login';
+import Home from './Pages/Home/Home';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div className="App">
-     
+
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+      </Routes>
+      
     </div>
   );
 }
