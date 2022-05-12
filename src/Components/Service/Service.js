dbff69b9d55f70df571b0f004d5a1f31eca63f31
arnobtarafder <rmtarnob@gmail.com>
2022-05-12 15:11:20 +0600

    add: services card  with styles at appointment page

---
+++ src/Components/Service/Service.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Service = ({service}) => {
+    const {name, slots} = service;
+
+    return (
+        <div>
+            <div class="card lg:mx-w-lg bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="card-title">{name}</h2>
+                    <p>{
+                        slots.length > 0 
+                        ? <span>{slots[0]}</span>
+                        : <span className='text-red-500'>Try another day</span>
+                        }</p>
+                    <p>{slots.length} {slots.length > 1 ? "spaces" : "space"} available</p>
+                    <div class="card-actions justify-end">
+                        <button disabled={slots.length === 0} class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
