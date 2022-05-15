40670eeb74710ed578d2f406de6b25fe8467a9c6
arnobtarafder <rmtarnob@gmail.com>
2022-05-15 13:33:27 +0600

    add: dynamic route for dashboard from navbar

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -5,15 +5,14 @@ const Dashboard = () => {
     return (
         <div>
             <div class="drawer drawer-mobile">
-                <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+                <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
                 <div class="drawer-content flex flex-col">
                     <h2 className='text-3xl text-purple-500'>Welcome to your Dashboard</h2>
                     <Outlet />
-                    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
 
                 </div>
                 <div class="drawer-side">
-                    <label for="my-drawer-2" class="drawer-overlay"></label>
+                    <label for="dashboard-sidebar" class="drawer-overlay"></label>
                     <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
                         <li><Link to="/dashboard">My Appointments</Link></li>
                         <li><Link to="/dashboard/reviews">My Reviews</Link></li>
