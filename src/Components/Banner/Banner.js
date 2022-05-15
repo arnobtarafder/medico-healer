407bd511f13fd18d9b9fbdd7519a8f657a1c9346
arnobtarafder <rmtarnob@gmail.com>
2022-05-15 13:41:11 +0600

    make home page banner button dynamic

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import chair from "../../assets/images/chair.png";
 import bg from "../../assets/images/bg.png"
 import PrimaryButton from '../../Components/PrimaryButton/PrimaryButton';
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
   return (
@@ -11,12 +12,14 @@ const Banner = () => {
 
       <div className="hero-content flex-col lg:flex-row-reverse">
         <img src={chair} className="max-w-lg rounded-lg shadow-2xl hidden lg:block" alt="" />
-       
+
         <div className='px-auto lg:pr-24'>
           <h1 className="text-5xl font-medium">Your New Smile Starts Here</h1>
 
           <p className="py-6">Book video call appointments with the country's most qualified specialist doctors, in their private chambers.</p>
-          <PrimaryButton>Appointment Now</PrimaryButton>
+
+          <Link to="/appointment"><PrimaryButton>Appointment Now</PrimaryButton>
+          </Link>
         </div>
       </div>
 
