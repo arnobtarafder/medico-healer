a937065b4a0c2108107077e07d0319bb1e48b8dc
arnobtarafder <rmtarnob@gmail.com>
2022-05-20 11:49:23 +0600

    add: use query functions at manage doctors

--- src/Components/ManageDoctors/ManageDoctors.js
+++ src/Components/ManageDoctors/ManageDoctors.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 
 const ManageDoctors = () => {
+    const {data: doctors, isLoading} = useQuery("doctors", () => fetch("", {
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+
+        }
+    }).then(res => res.json()));
+
+
     return (
         <div>
-            <h2>Manage Doctors</h2>
+            <h2 className='text-2xl'>Manage Doctors: {doctors.length}</h2>
         </div>
     );
 };
