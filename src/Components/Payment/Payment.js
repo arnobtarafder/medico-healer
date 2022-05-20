da0fcea61dba3bee40cfa1b8214a11c40572f721
arnobtarafder <rmtarnob@gmail.com>
2022-05-20 19:49:00 +0600

    add: simple payment card

--- src/Components/Payment/Payment.js
+++ src/Components/Payment/Payment.js
@@ -4,23 +4,37 @@ import { useParams } from 'react-router-dom';
 import Loading from '../Loading/Loading';
 
 const Payment = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     const url = `http://localhost:5000/booking/${id}`;
 
-    const {data: appointment, isLoading} = useQuery(["booking", id], () => fetch(url, {
+    const { data: appointment, isLoading } = useQuery(["booking", id], () => fetch(url, {
         method: 'GET',
         headers: {
             'authorization': `Bearer ${localStorage.getItem('accessToken')}`
         }
     }).then(res => res.json()));
 
-    if(isLoading) {
+    if (isLoading) {
         return <Loading />
     }
 
     return (
-        <div>
-            <h2 className="text-2xl text-purple-600">Please Pay for: {id}</h2>
+        <div className='mx-auto'>
+            <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+                <div class="card-body">
+                    <p className='text-success'>Hello, {appointment?.patientName}</p>
+                    <h2 class="card-title">Pay for: {appointment?.treatment}</h2>
+                    <p>Your appointment: <span className='text-orange-700'>{appointment?.date} </span> at {appointment?.slot}</p>
+                    <p>Please pay: ${appointment?.price}</p>
+                </div>
+            </div>
+            <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div class="card-body">
+
+
+
+                </div>
+            </div>
         </div>
     );
 };
