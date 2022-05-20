2be11d079a5da4847286890d8a76a6499b43933f
arnobtarafder <rmtarnob@gmail.com>
2022-05-20 19:19:07 +0600

    add: loading at payment page

--- src/Components/Payment/Payment.js
+++ src/Components/Payment/Payment.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import Loading from '../Loading/Loading';
 
 const Payment = () => {
     const {id} = useParams();
     const url = `http://localhost:5000/booking/${id}`;
 
-    const {data: appointment} = useQuery(["booking", id], () => fetch(url, {
+    const {data: appointment, isLoading} = useQuery(["booking", id], () => fetch(url, {
         method: 'GET',
         headers: {
             'authorization': `Bearer ${localStorage.getItem('accessToken')}`
         }
-    }).then(res => res.json()))
+    }).then(res => res.json()));
+
+    if(isLoading) {
+        return <Loading />
+    }
 
     return (
         <div>
