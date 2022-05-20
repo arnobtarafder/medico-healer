2c89462504a4ec2b0709c0c696945f31d02271ea
arnobtarafder <rmtarnob@gmail.com>
2022-05-20 11:58:56 +0600

    send data from manage doctors to doctor row

--- src/Components/ManageDoctors/ManageDoctors.js
+++ src/Components/ManageDoctors/ManageDoctors.js
@@ -1,22 +1,48 @@
 import React from 'react';
 import { useQuery } from 'react-query';
+import DoctorRow from '../DoctorRow/DoctorRow';
 import Loading from '../Loading/Loading';
 
 const ManageDoctors = () => {
-    const {data: doctors, isLoading} = useQuery("doctors", () => fetch("http://localhost:5000/doctors", {
+    const { data: doctors, isLoading } = useQuery("doctors", () => fetch("http://localhost:5000/doctors", {
         headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
 
         }
     }).then(res => res.json()));
 
-    if(isLoading) {
+    if (isLoading) {
         return <Loading />
     }
 
     return (
         <div>
-            <h2 className='text-2xl'>Manage Doctors: {doctors.length}</h2>
+            <h2 className='text-2xl'>Manage Doctors: {doctors?.length}</h2>
+
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Specialty</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors.map((doctor, index) => <DoctorRow
+                            key={doctor._key}
+                            doctor={doctor}
+                            index={index}
+                            >
+
+                            </DoctorRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
