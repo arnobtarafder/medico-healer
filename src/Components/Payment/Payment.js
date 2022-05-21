c82a7beeb1777db8adcce7ff9c58d1bf57a12cfb
arnobtarafder <rmtarnob@gmail.com>
2022-05-21 17:28:24 +0600

    add: payment successability scores

--- src/Components/Payment/Payment.js
+++ src/Components/Payment/Payment.js
@@ -27,18 +27,18 @@ const Payment = () => {
 
     return (
         <div className='mx-auto'>
-            <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
-                <div class="card-body">
+            <div className="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+                <div className="card-body">
                     <p className='text-success'>Hello, {appointment?.patientName}</p>
-                    <h2 class="card-title">Pay for: {appointment?.treatment}</h2>
+                    <h2 className="card-title">Pay for: {appointment?.treatment}</h2>
                     <p>Your appointment: <span className='text-orange-700'>{appointment?.date} </span> at {appointment?.slot}</p>
                     <p>Please pay: ${appointment?.price}</p>
                 </div>
             </div>
-            <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
-                <div class="card-body">
+            <div className="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div className="card-body">
                     <Elements stripe={stripePromise}>
-                        <CheckoutForm />
+                        <CheckoutForm appointment={appointment} />
                     </Elements>
                 </div>
             </div>
