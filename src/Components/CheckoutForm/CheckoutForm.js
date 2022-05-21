bcb6288a3979159c3eccbef575cb10650cdc857b
arnobtarafder <rmtarnob@gmail.com>
2022-05-21 17:32:59 +0600

    add; confirmation of card payment transaction with send data

--- src/Components/CheckoutForm/CheckoutForm.js
+++ src/Components/CheckoutForm/CheckoutForm.js
@@ -28,86 +28,110 @@ const CheckoutForm = ({ appointment }) => {
 
     }, [price])
 
-const handleSubmit = async event => {
-    event.preventDefault();
+    const handleSubmit = async event => {
+        event.preventDefault();
 
 
-    if (!stripe || !elements) {
+        if (!stripe || !elements) {
 
-        return;
-    }
+            return;
+        }
 
 
-    const card = elements.getElement(CardElement);
+        const card = elements.getElement(CardElement);
 
-    if (card == null) {
-        return;
-    }
+        if (card == null) {
+            return;
+        }
 
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-        type: 'card',
-        card,
-    });
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
 
-    setCardError(error?.message || "");
+        setCardError(error?.message || "");
 
-      // confirm card payment
-      const { paymentIntent, error: intentError } = await stripe.confirmCardPayment(
-        clientSecret,
-        {
-            payment_method: {
-                card: card,
-                billing_details: {
-                    name: patientName,
-                    email: patient
+        // confirm card payment
+        const { paymentIntent, error: intentError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: patientName,
+                        email: patient
+                    },
                 },
             },
-        },
-    );
+        );
 
-    if (intentError) {
-        setCardError(intentError?.message);
-        setProcessing(false);
+        if (intentError) {
+            setCardError(intentError?.message);
+            setProcessing(false);
+        }
+        else {
+            setCardError('');
+            setTransactionId(paymentIntent.id);
+            console.log(paymentIntent);
+            setSuccess('Congrats! Your payment is completed.')
+
+            //store payment on database
+            const payment = {
+                appointment: _id,
+                transactionId: paymentIntent.id
+            }
+            fetch(`http://localhost:5000/booking/${_id}`, {
+                method: 'PATCH',
+                headers: {
+                    'content-type': 'application/json',
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                },
+                body: JSON.stringify(payment)
+            }).then(res => res.json())
+                .then(data => {
+                    setProcessing(false);
+                    console.log(data);
+                })
+        }
+
     }
-    else {
-        setCardError('');
-        setTransactionId(paymentIntent.id);
-        console.log(paymentIntent);
-        setSuccess('Congrats! Your payment is completed.')
-    }
-
-}
 
 
 
-return (
-    <>
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe || !clientSecret}>
-                Pay
-            </button>
-        </form>
-        {
-            cardError && <p className='text-red-500'>{cardError}</p>
-        }
-    </>
-);
+                    }}
+                />
+                <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe || !clientSecret}>
+                    Pay
+                </button>
+            </form>
+            {
+                cardError && <p className='text-red-500'>{cardError}</p>
+            }
+            {
+                success && <div className='text-green-500'>
+                    <p>{success}  </p>
+                    <p>Your transaction Id: <span className="text-orange-500 font-bold">{transactionId}</span> </p>
+                </div>
+            }
+        </>
+    );
 };
 
 export default CheckoutForm;
